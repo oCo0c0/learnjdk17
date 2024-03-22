27243321b7b653eb792e4d40e01dd52d1f2f546a
王天龙 <wangtianlong@gigacloudtech.com>
2024-03-22 17:45:50 +0800

    jdk17-version-1

---
+++ src/jdk.jshell/jdk/internal/jshell/tool/resources/version.java
@@ -0,0 +1,11 @@
+package jdk.internal.jshell.tool.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "17.0.9+11-LTS-201" },
+            { "jdk", "17.0.9" },
+            { "release", "17.0.9" },
+        };
+    }
+}
